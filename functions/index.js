2f7edc101017217d9059536e7b35b2ba4e7355f0
Lucas Teles <lucasvst@gmail.com>
2024-09-29 04:02:49 -0300

    updates

--- functions/index.js
+++ functions/index.js
@@ -5,24 +5,27 @@ const { onRequest } = require("firebase-functions/v2/https");
 exports.postLead = onRequest(
     {
       region: ["us-east1"],
-      cors: ["educacau.com.br"],
+      cors: true,
     },
     async (req, res) => {
 
-      const lead = {
-        "name": "Nome teste",
-        "phone": "47996718611",
-      }
+      const lead = req.body
 
       try {
         await fetch("https://us-east1-educacau-ae429.cloudfunctions.net/ext-http-export-sheets-saveRecord", {
           method: "POST",
-          body: JSON.stringify(lead)
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(lead),
         })
       } catch (e) {
         logger.error("Some error occurred while trying to save lead:", lead);
+        res.status(500).send()
+        return
       }
 
-      res.status(200).send();
+      res.status(200).send()
     },
 );
